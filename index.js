dd31d7370445053747a85554db5702a9c54f33d9
Victorriv <Victorrivera714@yahoo.com>
2024-10-20 06:50:14 -0700

    practicing arithmetic

--- index.js
+++ index.js
@@ -81,6 +81,9 @@ let students = 30;
 
 
 
-students = students + 1;
+// students = students + 1;
+// students = students -1;
+// students = students * 2;
+students = students / 2;
 
 console.log(students);
